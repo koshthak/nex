f508502b65bbbc185747d3ce77508987edcc9ebf
yadav-saurabh <yadav-saurabh@outlook.com>
2020-04-30 10:56:03 +0530

    removed unused files

---
+++ config/index.js
@@ -0,0 +1,5 @@
+const corsConfig = require('./cors');
+const ddosConfig = require('./ddos');
+const dbConfig = require('./database');
+
+module.exports = { corsConfig, ddosConfig, dbConfig };
--- middlewares/rateLimitter.js
+++
@@ -1,1 +0,0 @@
-// For block IP(s) for bruteforce attacks OR limit continuous requests from same IP(s)
